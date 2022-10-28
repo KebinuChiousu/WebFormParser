94a9261ba87108a5a4b9830e89d1bb586cd57cc9
Kiera Meredith <kiera@meredithkm.info>
2022-10-28 16:23:52 -0400

    Add Model

---
+++ WebFormParser/model/Source.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebFormParser.model
+{
+    public static class Source
+    {
+        public static string source = "";
+        public static bool changed = false;
+        public static bool addJsParserToHeader = false;
+        public static bool addJsParserDotJs = false;
+        public static string embeddedJsScript = string.Empty;
+        internal static string error = "\n\nERROR LOG: \n";
+        public static string filename = string.Empty;
+        public static List<string> anonymouseFunctions = new List<string>();
+    }
+
+}
