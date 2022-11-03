007e2917f94f2513cacb2078f5fd519e6f60d034
Kiera Meredith <kiera@meredithkm.info>
2022-11-03 18:31:40 -0400

    Add Demo Mode to Program

---
+++ WebFormParser/Utility/Demo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace WebFormParser.Utility
+{
+    public static class Demo
+    {
+        public static void PrintClassicNodes()
+        {
+            
+            string input = Util.GetEmbeddedString("ClassicAspMigration.asp");
+
+            var entries = Asp.Parser.GetRegexGroupMatches(input);
+
+            foreach (var entry in entries)
+            {
+                Console.WriteLine($"Group {entry.GroupName}");
+                Console.WriteLine($"Value: {entry.Value}");
+            }
+        }
+    }
+}
---
+++ WebFormParser/Utility/Options/DemoOptions.cs
@@ -0,0 +1,15 @@
+﻿using CommandLine;
+
+namespace WebFormParser.Utility.Options;
+
+public class DemoOptions
+{
+    public DemoOptions()
+    {
+        Demo = false;
+    }
+
+    [Option("demo", HelpText = "Demo Mode", Required = false)]
+    public bool Demo { get; set; }
+
+}
