6b8d28de9ba8a002bd6d32bc953d285c4fcce775
Kiera Meredith <kiera@meredithkm.info>
2022-12-05 19:49:30 -0500

    Clean-Up parser.

--- WebFormParser/Utility/Demo.cs
+++ WebFormParser/Utility/Demo.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using AngleSharp.Dom;
 using Microsoft.VisualBasic;
+using WebFormParser.Utility.Asp.Enum;
 
 namespace WebFormParser.Utility
 {
@@ -18,10 +20,25 @@ namespace WebFormParser.Utility
 
             var entries = Asp.Parser.GetRegexGroupMatches(input);
 
+            var htmlList = Asp.CodeGen.Generate(ref entries, AspFileEnum.Html);
+            var codeList = Asp.CodeGen.Generate(ref entries);
+
+            /*
+            Console.WriteLine("ASP Node Tree:");
+
             foreach (var entry in entries)
             {
-                Console.WriteLine($"Group {entry.GroupName}");
+                Console.WriteLine($"FileType: {entry.GetFileType(entry.FileType)}");
+                Console.WriteLine($"Group: {entry.GroupName}");
                 Console.WriteLine($"Value: {entry.Value}");
+            }
+            */
+
+            Console.WriteLine("Html: ");
+
+            foreach (var entry in htmlList)
+            {
+                Console.WriteLine(entry);
             }
         }
     }
