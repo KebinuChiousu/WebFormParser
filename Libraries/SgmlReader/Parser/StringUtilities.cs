8715a5586a2cf6c3670383b3bef42ee59ca674ac
Kiera Meredith <kiera@meredithkm.info>
2022-12-06 07:20:18 -0500

    Remove Un-needed Libraries

--- Libraries/SgmlReader/Parser/StringUtilities.cs
+++
@@ -1,9 +0,0 @@
-namespace Sgml;
-
-internal static class StringUtilities
-{
-    public static bool EqualsIgnoreCase(string a, string b)
-    {
-        return string.Equals(a, b, StringComparison.OrdinalIgnoreCase);
-    }
-}
