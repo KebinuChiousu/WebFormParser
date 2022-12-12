2233631b2be43b79d346e14d7bff5d1ff57dd2f4
Kiera Meredith <kiera@meredithkm.info>
2022-12-12 18:34:04 -0500

    Create Attribute Class to handle Attribute Render.

---
+++ WebFormParser/Utility/Asp/Attr.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebFormParser.Utility.Asp
+{
+    public class Attr
+    {
+        public readonly string Value;
+        public readonly bool IsValue;
+
+        public Attr(string value, bool isValue)
+        {
+            this.Value = value;
+            this.IsValue = isValue;
+        }
+    }
+}
