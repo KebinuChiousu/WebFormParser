bfce62a6ffeba9a841e7bec2120b8b8dcd104bcc
Kiera Meredith <kiera@meredithkm.info>
2022-12-12 18:35:57 -0500

    Implement new parser based on HtmlAgilityPack.

--- WebFormParser/Utility/Asp/Parser.cs
+++ WebFormParser/Utility/Asp/Parser.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using WebFormParser.model;
 using WebFormParser.Utility.Asp.Enum;
 
@@ -11,238 +16,305 @@ namespace WebFormParser.Utility.Asp
 {
     public static class Parser
     {
-        private const string Pattern = @"(?'open'<[a-zA-Z]+\s[0-9A-Za-z=""' ]+|<[A-Za-z]+>|<[A-Za-z]+\s)|((?'code'<%\n([^%>]|\n)+%>|<%([^%>]|\n)+%>)|(?'attr'[a-zA-Z0-9]+|[0-9A-Za-z=""']+))|(?'close'>|>([^<]|\n)+</[A-Za-z]+>|/>|[0-9A-Za-z=""'\n/ ]+>|</[A-Za-z]+>)|(?'comment'<!--.+-->|<!--[\s\S.]+-->)";
-        private const RegexOptions Options = RegexOptions.Multiline;
+        private const RegexOptions Options = RegexOptions.Multiline | RegexOptions.Compiled;
 
-        public static List<Entry> GetRegexGroupMatches(string input)
+        public static List<Entry> ParseDocument(string input)
         {
-            var ret = new List<Entry>();
+            var htmlDoc = new HtmlAgilityPack.HtmlDocument();
+            htmlDoc.LoadHtml(input);
+            var htmlNodes = htmlDoc.DocumentNode.ChildNodes;
             var nodes = new List<Entry>();
-            var regex = new Regex(Pattern, Options);
-            var groupList = GetRegexGroupNames(regex);
-            var mc = regex.Matches(input); 
-            foreach (Match m in mc)
+
+            var state = new State();
+
+            foreach (var node in htmlNodes)
             {
-                var groupName = GetGroupNameForMatch(groupList, m);
-                var matchValue = FormatValue(m.Value, groupName);
-                var entry = new Entry
-                {
-                    GroupName = groupName,
-                    Value = matchValue
-                };
-                nodes.Add(entry);
-            }
-
-            nodes = CategorizeEntries(nodes);
-
-            ret = MergeNodes(nodes);
-            
-            return ret;
-        }
-
-        private static List<Entry> MergeNodes(List<Entry> nodes)
-        {
-            for(var i = 0; i < nodes.Count; i++)
-            {
-                var node = nodes[i];
-                var prevNode = i > 0 ? nodes[i - 1] : null;
-
-                if (prevNode == null )
-                    continue;
-
-                if (node.TagType is not (TagTypeEnum.Attr or TagTypeEnum.CodeValue))
-                    continue;
-
-                if (prevNode.TagType is not (TagTypeEnum.Attr or TagTypeEnum.CodeValue or TagTypeEnum.CodeOpen or TagTypeEnum.CodeContent))
-                    continue;
-
-                if (prevNode.TagType is (TagTypeEnum.CodeOpen or TagTypeEnum.CodeContent))
-                { ;
-                    node.TagType = TagTypeEnum.CodeContent;
-                    node.GroupName = Entry.GetTagType(node.TagType);
-                }
-
-                if (prevNode.TagType == TagTypeEnum.CodeOpen)
-                    continue;
-
-                prevNode.Value += (node.TagType == TagTypeEnum.CodeContent) ? " " + node.Value : node.Value;
-
-                nodes.RemoveAt(i);
-                i--;
+                HandleNode(ref nodes, ref state, node);
             }
 
             return nodes;
         }
 
-        private static List<Entry> CategorizeEntries(List<Entry> entries)
+        private static void HandleNode(ref List<Entry> entries, ref State state, HtmlNode node)
         {
-            var ret = new List<Entry>();
-            var state = new State();
+            Entry closingTag;
+            var block = GetValue(node);
 
-            foreach (var entry in entries)
-            {
+            if (string.IsNullOrEmpty(block))
+                return;
 
-                state.PrevCode = state.IsCode;
-                
-                switch (entry.GroupName)
-                {
-                    case "open":
-                        state.IsTag = true;
-                        state.IsOpen = !entry.Value.Contains(">");
-                        break;
-                    case "close":
-                        state.IsTag = false;
-                        state.IsOpen = false;
-                        break;
-                    case "code":
-                        state.IsCode = true;
-                        break;
-                }
+            var entry = new Entry();
 
-                var tag = HandleEntry(entry, ref state);
+            if (IsCode(block)) state.IsCode = true;
 
-                ret.Add(tag);
-            }
-
-            return ret;
-        }
-
-        private static Entry HandleEntry(Entry entry, ref State state)
-        {
+            entry.TagType = GetTagType(node, state);
+            entry.Value = FormatValue(entry, block);
+            entry.GroupName = Entry.GetGroupName(entry.TagType);
             entry.FileType = state.IsCode ? AspFileEnum.CodeBehind : AspFileEnum.Html;
 
-            SetTagType(ref entry, state);
+            if (state.IsCode)
+                ProcessCodeBlock(ref entry, ref state, node);
 
-            switch (entry.GroupName)
+            entry.IsOpen = node.HasAttributes;
+            entry.HasAttr = node.HasAttributes;
+
+            entries.Add(entry);
+
+            if (node.HasAttributes) ProcessAttributes(ref entries, ref state, node);
+
+            if (!node.HasChildNodes)
             {
-                case "attr":
-                    if (!state.IsOpen) entry.TagType = TagTypeEnum.Content;
-                    if (state.IsCode)
-                    {
-                        entry.TagType = state.IsOpen ? TagTypeEnum.CodeAttr : TagTypeEnum.CodeContent;
-                        entry.TagType = InCodeBlock(entry); 
-                        entry.CodeFunction = "page_logic_" + state.FuncCount.ToString("D2");
-                    }
-                    break;
-                case "code":
-                    if (state.IsOpen)
-                    {
-                        entry.TagType = TagTypeEnum.CodeAttr;
-                        if (!state.PrevCode)
-                            state.FuncCount++;
-                        entry.CodeFunction = "page_logic_" + state.FuncCount.ToString("D2");
-                    }
-                    if (entry.Value.Contains("<%@"))
-                    {
-                        entry.TagType = TagTypeEnum.Page;
-                        entry.FileType = AspFileEnum.Html;
-                        state.IsCode = false;
-                    }
+                if (!node.HasAttributes)
+                    return;
 
-                    if (entry.FileType == AspFileEnum.CodeBehind)
-                    {
-                        if (entry.TagType == TagTypeEnum.Content)
-                        {
-                            entry.TagType = TagTypeEnum.CodeContent;
-                            if (!state.PrevCode)
-                                state.FuncCount++;
-                        }
-
-                        entry.CodeFunction = "page_logic_" + state.FuncCount.ToString("D2");
-                    }
-                    break;
-                case "open":
-                    if (state.IsCode)
-                    {
-                        entry.TagType = TagTypeEnum.CodeOpen;
-                        if (!state.PrevCode)
-                            state.FuncCount++;
-                        entry.CodeFunction = "page_logic_" + state.FuncCount.ToString("D2");
-                    }
-                    break;
-                case "close":
-                    if (state.IsCode)
-                    {
-                        entry.TagType = TagTypeEnum.CodeClose;
-                        entry.CodeFunction = "page_logic_" + state.FuncCount.ToString("D2");
-                    }
-                    break;
+                CloseOpenTag(ref entries, entry, state);
+                if (entry.SelfClosing())
+                    return;
+                
+                closingTag = GetClosingTag(node.Name, state);
+                entries.Add(closingTag);
             }
 
-            SetGroupName(ref entry);
+            if (node.HasAttributes)
+                CloseOpenTag(ref entries, entry, state);
+            
+            foreach (var child in node.ChildNodes)
+            {
+                HandleNode(ref entries, ref state, child);
+            }
+            
+            closingTag = GetClosingTag(node.Name, state);
+            entries.Add(closingTag);
+        }
 
-            if (!state.IsCode)
-                return entry;
+        private static void CloseOpenTag( ref List<Entry> entries, Entry node, State state)
+        {
+            bool closed = node.SelfClosing();
+            
+            var entry = new Entry
+            {
+                Value = closed ? "/>" : ">",
+                TagType = Entry.GetTagType(TagTypeEnum.Close, state.IsCode),
+                IsOpen = !closed
+            };
+            entry.GroupName = Entry.GetGroupName(entry.TagType);
+            entries.Add(entry);
+        }
 
-            state.OpenCode += entry.Value.Count(f => f == '{');
-            state.CloseCode += entry.Value.Count(f => f == '}');
-            state.HandleCodeState(entry.Value);
+        private static void ProcessAttributes(ref List<Entry> entries, ref State state, HtmlNode node)
+        {
+            foreach (var attr in node.Attributes)
+            {
+                ProcessAttr(ref entries, ref state, attr);
+            }
+        }
+
+        private static void ProcessAttr(ref List<Entry> entries, ref State state, HtmlAttribute attr)
+        {
+
+            Entry? entry = null;
+            var result = string.Empty;
+            
+            bool check1 = IsCode(attr.Name); 
+            bool check2 = IsCode(attr.Value);
+
+            bool isCode = check1 || check2;
+
+            var blocks = new List<Attr>();
+
+            if (check1)
+                blocks.AddRange(GetBlocks(attr.Name, true));
+            else
+            {
+                var value = attr.Name + "=";
+                entry = AddAttr(value, isCode, true, ref state);
+                if (entry != null && isCode) entries.Add(entry);
+            }
+            
+            if (check2)
+                blocks.AddRange(GetBlocks(attr.Value, false));
+            else
+            {
+                var value = attr.Value;
+                if (isCode)
+                    entry = AddAttr(value, isCode, false, ref state);
+                else
+                    if (entry != null)
+                        entry.Value += $"\"{value}\"";
+                
+                if (entry != null) entries.Add(entry);
+            }
+
+            foreach (var block in blocks)
+            {
+                var isCodeBlock = IsCode(block.Value);
+                var isCodeCheck = isCodeBlock || state.IsCode;
+                entry = AddAttr(block.Value, isCodeCheck, !block.IsValue, ref state);
+                if (entry != null) entries.Add(entry);
+            }
+        }
+
+        private static Entry? AddAttr(string value, bool isCode, bool isAttr, ref State state)
+        {
+            if (string.IsNullOrEmpty(value))
+                return null;
+
+            var entry = new Entry
+            {
+                Value = value,
+                FileType = isCode ? AspFileEnum.CodeBehind : AspFileEnum.Html,
+                TagType = isAttr ? TagTypeEnum.Attr : TagTypeEnum.Value,
+                CodeFunction = isCode ? "page_logic_" + state.FuncCount.ToString("D2") : null,
+                IsOpen = true
+            };
+
+            entry.TagType = Entry.GetTagType(entry.TagType, isCode);
+            entry.GroupName = Entry.GetGroupName(entry.TagType);
+
+            state.IsCode = isCode;
+            HandleCodeState(ref state, entry.Value);
 
             return entry;
         }
 
-        private static TagTypeEnum InCodeBlock(Entry entry)
+        [System.Diagnostics.DebuggerStepThrough()]
+        private static IEnumerable<Attr> GetBlocks(string block, bool isAttr)
         {
-            var isCode = entry.Value.Contains("<%") || entry.Value.Contains("%>");
-            return isCode ? TagTypeEnum.CodeAttr : TagTypeEnum.CodeValue;
-        }
-
-        private static void SetGroupName(ref Entry entry)
-        {
-            entry.GroupName = Entry.GetTagType(entry.TagType);
-        }
-
-        private static void SetTagType(ref Entry entry, State state)
-        {
-            entry.TagType = entry.GroupName switch
+            const string pattern = @"<%[^%>]+%>";
+            var regex = new Regex(pattern, Options);
+            var mc = regex.Matches(block);
+            foreach (Match m in mc)
             {
-                "attr" => TagTypeEnum.Attr,
-                "open" => TagTypeEnum.Open,
-                "close" => TagTypeEnum.Close,
-                "comment" => TagTypeEnum.Comment,
-                _ => TagTypeEnum.Content
+                block = block.Replace(m.Value, "<sep>" + m.Value + "<sep>");
+            }
+
+            var blocks = block.Split("<sep>").ToList();
+
+            var result = new List<Attr>();
+
+            foreach (var t in blocks)
+            {
+                if (string.IsNullOrEmpty(t))
+                    continue;
+
+                result.Add(new Attr(t, !isAttr));
+            }
+
+            return result;
+        }
+
+        private static void ProcessCodeBlock(ref Entry entry, ref State state, HtmlNode node)
+        {
+            var block = entry.Value;
+            var begIdx = block.IndexOf("<%") - 1;
+            if (begIdx > 0) block = block.Substring(begIdx);
+            var endIdx = block.IndexOf("%>") + 1;
+            if (endIdx > 0) block = block.Substring(1, endIdx);
+            entry.Value = block;
+
+            entry.CodeFunction = "page_logic_" + state.FuncCount.ToString("D2");
+
+            if (node.NodeType == HtmlNodeType.Text)
+            {
+                entry.TagType = TagTypeEnum.CodeContent;
+                entry.GroupName = Entry.GetGroupName(entry.TagType);
+            }
+
+            HandleCodeState(ref state, entry.Value);
+        }
+
+        private static Entry GetClosingTag(string name, State state)
+        {
+            var tagType = state.IsCode ? TagTypeEnum.CodeClose : TagTypeEnum.Close;
+
+            var entry = new Entry()
+            {
+                Value = name,
+                TagType = tagType,
+                GroupName = Entry.GetGroupName(tagType),
+                FileType = state.IsCode ? AspFileEnum.CodeBehind : AspFileEnum.Html
             };
 
-            entry.IsOpen = state.IsOpen;
+            if (state.IsCode)
+                entry.CodeFunction = "page_logic_" + state.FuncCount.ToString("D2");
+
+            return entry;
+        }
+        private static void HandleCodeState(ref State state, string block)
+        {
+            state.OpenCode += block.Count(f => f == '{');
+            state.CloseCode += block.Count(f => f == '}');
+            state.HandleCodeState(block);
         }
 
-        private static List<string> GetRegexGroupNames(Regex regex)
+        private static string FormatValue(Entry entry, string value)
         {
-            var groupList = regex.GetGroupNames().ToList();
-            return groupList.Where( t => !int.TryParse( t , out var i)).ToList();
-        }
-
-        private static string GetGroupNameForMatch(List<string> groupNames, Match m)
-        {
-            var ret = string.Empty;
-            var groupList = m.Groups.Keys.Where(t => m.Groups[t].Success).Where(t => !int.TryParse(t, out var i)).ToList();
-            if (groupList.Count > 0)
-                ret = groupList[0];
-
-            return ret;
-        }
-
-        private static string FormatValue(string value, string groupName)
-        {
-            switch (groupName)
+            var ret = value;
+            switch (entry.TagType)
             {
-                case "code":
-                    return FormatCode(value);
-                case "comment":
-                    return FormatCode(value);
+                case TagTypeEnum.Page:
+                    ret = value.Replace(Environment.NewLine, "");
+                    return ret;
                 default:
-                    return value;
+                    return ret;
             }
         }
 
-        private static string FormatCode(string value)
+        private static string GetValue(HtmlNode node)
         {
-            var lineList = new List<string>();
-            var lines = value.Split(Environment.NewLine);
-            foreach(var line in lines)
-                lineList.Add(line.Trim());
-            return string.Join(" ", lineList);
+            var pattern = @"[\p{C}-[\r\n\t]]+";
+            var value = string.Empty;
+            
+            switch (node.NodeType)
+            {
+                case HtmlNodeType.Comment:
+                    value = node.InnerHtml;
+                    break;
+                case HtmlNodeType.Element:
+                    value = node.Name;
+                    break;
+                default:
+                    value = node.InnerText;
+                    break;
+            }
+
+            value = Regex.Replace(value, pattern, string.Empty);
+            if (value == Environment.NewLine) value = string.Empty;
+
+            return value;
         }
 
+        private static TagTypeEnum GetTagType(HtmlNode node, State state)
+        {
+            switch (node.NodeType)
+            {
+                case HtmlNodeType.Comment:
+                    return state.IsCode ? TagTypeEnum.CodeComment : TagTypeEnum.Comment;
+                case HtmlNodeType.Text:
+                    return CategorizeText(node, state);
+                case HtmlNodeType.Element:
+                    return state.IsCode ? TagTypeEnum.CodeOpen : TagTypeEnum.Open;
+                default:
+                    return state.IsCode ? TagTypeEnum.CodeContent : TagTypeEnum.Content;
+            }
+        }
+
+        private static TagTypeEnum CategorizeText(HtmlNode node, State state)
+        {
+            var block = node.InnerText;
+            if (block.Contains("<%@")) return TagTypeEnum.Page;
+            return TagTypeEnum.Content;
+        }
+
+        private static bool IsCode(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                return false;
+            
+            var isCode = value.Contains("<%") || value.Contains("%>");
+            var isPage = value.Contains("<%@");
+
+            return isCode && !isPage;
+        }
     }
 }
